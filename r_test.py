7c2847a42bd645a433029dcfbf83b4f5357b846c
vensaherald <>
2018-10-12 12:18:40 +0100

    further learning requests with redirects and httpbin.org

--- r_test.py
+++ r_test.py
@@ -1,11 +1,20 @@
 from __future__ import print_function
-import requests
+from requests import request
 #using "https://httpbin.org/redirect/n" to follow redirections through 
 #test r = requests.get(url, allow_redirects=False)
 #r.headers['Location'] is the next url
-n = 3
-url = 'https://httpbin.org/redirect/{}'.format(n)
-r = requests.request('GET',url, allow_redirects=False)
-print (r.status_code)
+n = 9
+redir = 302
+host = 'https://httpbin.org'
+url = '{}/redirect/{}'.format(host,n)
+r = request('GET',url, allow_redirects=False)
+redir = r.status_code
+while (redir == 302):
+	print (r.status_code)
+	url = '{}{}'.format(host,r.headers['Location'])
+	r = request('GET',url, allow_redirects=False)
+	redir = r.status_code
+	
+	
 
-print (r.headers)
+print ("end")
